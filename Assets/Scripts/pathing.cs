ca2ea6c7d8f97ee85934a526bed17df8368d5457
Tangel2002 <90701100+Tangel2002@users.noreply.github.com>
2022-03-15 04:57:23 -0400

    prefab enemies can follow the track
    track is prefabbed and pathing script takes the waypoint parent Track Points and runs through the children like an array

--- Assets/Scripts/pathing.cs
+++ Assets/Scripts/pathing.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class pathing : MonoBehaviour
 {
 
-
-    public Transform[] waypoints;
+    public GameObject TrackPoints;
+    Transform[] waypoints;
+    private void Start()
+    {
+        waypoints = TrackPoints.GetComponentsInChildren<Transform>();
+    }
+    
 
     public float movespeed = 2f;
-    int index = 0;
+    int index = 1;
 
     // Update is called once per frame
     void Update()
     {
+        print(index);
+        
         if(index <= waypoints.Length - 1)
         {
             this.transform.position = Vector2.MoveTowards(transform.position, waypoints[index].position, movespeed * Time.deltaTime);
-
+            print(waypoints[index].name);
             if(this.transform.position == waypoints[index].position)
             {
                 index++;
