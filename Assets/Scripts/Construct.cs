4f546e2cb847465ce6d43b2e077a3112662efc09
Tangel2002 <90701100+Tangel2002@users.noreply.github.com>
2022-03-15 02:32:28 -0400

    collision detect
    contructs cannot be placed on the track. soon they will check from other constructs

--- Assets/Scripts/Construct.cs
+++ Assets/Scripts/Construct.cs
@@ -8,17 +8,34 @@ public class Construct : MonoBehaviour
     public GameObject towerDecal;
     public GameObject tower;
     GameObject clone;
+    public PolygonCollider2D track;
 
+    bool obstructed = false;
     bool placed = true;
     // Update is called once per frame
     void Update()
     {
+        
         if (placed == false)
         {
             Vector2 adjustedPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
             clone.transform.position = adjustedPos;
         }
+        if (Input.GetMouseButtonDown(0) && placed == false)
+        {
+            if (clone.GetComponent<CircleCollider2D>().IsTouching(track))
+            {
+                obstructed = true;
+            }
+            else
+                obstructed = false;
+            if(obstructed == false)
+            {
+                Placed();
+            }
+        }
     }
+
 
 
     public void Place()
